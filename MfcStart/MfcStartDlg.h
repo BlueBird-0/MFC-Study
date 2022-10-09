b0d63537e954df3cd35385ed65dabc0420e276ec
bluebird <ij3512@naver.com>
2022-10-09 22:21:49 +0900

    feat: 프로젝트 생성/ 단순 계산 기능 추가
    'MfcStart.rc.' 메인화면에 'edittext' 및 'button' 구현
    'MfcStartDlg.cpp' 버튼 클릭 시 두 값을 더한 후 출력하는 기능 추가
    'ignore.git' 파일 추가 "gitignore.io->'visualstudio' "

---
+++ MfcStart/MfcStartDlg.h
@@ -0,0 +1,42 @@
+﻿
+// MfcStartDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CMfcStartDlg 대화 상자
+class CMfcStartDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMfcStartDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCSTART_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedBtnTest();
+	//변수 네이밍룰: (선언위치)_(자료형)(변수명)
+	int m_nNum1;
+	int m_nNum2;
+	afx_msg void OnEnChangeEditNum();
+	afx_msg void OnEnChangeEditNum2();
+	afx_msg void OnEnChangeEditNum1();
+};
